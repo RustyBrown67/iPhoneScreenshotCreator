505974ec4faad1dff6a1fc39a4c15c8bf67755d8
Russell Brown <russell.brown67@googlemail.com>
2017-12-27 19:37:57 +0000

    Initial Commit

---
+++ iPhoneScreenshotCreator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  iPhoneScreenshotCreator
+//
+//  Created by Russell Brown on 27/12/2017.
+//  Copyright © 2017 Russell Brown. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
